c365d610a4a62e2bf5852b8b38a8ebc94392a765
AwesomeNoob999 <51246943+AwesomeNoob999@users.noreply.github.com>
2022-03-16 19:40:18 -0500

    Add files via upload

--- FUNCTIONS.rb
+++ FUNCTIONS.rb
@@ -2,7 +2,7 @@ module Func
 def add_item
     puts "What item would you like to add? Please use format 'add {food*container*description}'"
     print ">>"
-    $devmode ? p("e") : item_add = gets.chomp
+    $devmode ? item_add="apple*bag" : item_add = gets.chomp
     item, container, desc = item_add.split("*")
     item = item.upcase.to_sym
     container = container.upcase.to_sym
@@ -11,21 +11,30 @@ end
 
 def dev_tools
     $devmode = true
-    
+    help_prompt
     #tests
     $devmode = false
 end
 
+def help_devtool
+    $iter += 1
+    m = "#{if $iter-1 < ACTIONS.size; "help" ;else "quit" end} #{if $iter - 1 <= ACTIONS.size; ACTIONS[$iter-1]; else "" end}"
+    puts "#{m}"
+    m
+end
 
 def help_prompt
+    $iter = 0
     puts "what command would you like help with? Please use format 'help {command}'"
     loop do
 	print ">>"
-	$devmode ? p("egg") : help_input = gets.chomp
+	$devmode ? help_input=help_devtool : help_input = gets.chomp
 	help_quit, command_modifier = help_input.split(" ")
 	if help_quit == "help"
 	    command_modifier = command_modifier.upcase.to_sym if command_modifier != nil
 	    case command_modifier
+		when :SAVE
+		    puts "allows you to save the changes to the file, your changes are also saved when using the quit command"
 		when :QUIT
 		    puts "quits the current task you are doing right now, if you arent doing a task it exits the program"
 		when :LIST
@@ -34,6 +43,18 @@ def help_prompt
 		    puts "displays this prompt"
 		when :DEVTOOLS
 		    puts "performs various tests to see if the program is functional"
+		when :PURGE
+		    puts "deletes all the information bout wha is in your freezer, DONT USE IF YOU DONT WANT TO DELETE EVERYTHING"
+		when :REMOVE_ITEMS_LIST
+		    puts "removes multiple items from your freezer"
+		when :REMOVE_ITEM
+		    puts "removes a single item from your freezer"
+		when :ADD_ITEMS_LIST
+		    puts "adds multiple items to your freezer"
+		when :ADD_ITEM
+		    puts "adds a single item to your freezer"
+		else
+		    puts "unknown command"
 	    end
 	elsif help_quit == "quit"
 	    break
@@ -58,16 +79,25 @@ def purge_start(file)
     end
 end
 
-def encrypt()
-
+def encrypt(buffer)
+    item = buffer.keys[0].to_s
+    value = buffer.values[0].values[0].to_s
+    container = buffer.values[0].values[1].to_s
+    desc = buffer.values[0].values[2]
+    "@#{item}*#{value}*{#{container}}*[#{desc}]"
 end
 
-def decrypt()
-
+def decrypt(freeze)
+    for thing in freeze
+	item, value, container, desc = thing.split("*")
+	item = item.sub("@","").to_sym
+	value = value.to_i
+	container = container.sub(/"{}"/)
+    end
 end
 
-def read(file)
-
+def save
+    
 end
 
 end
--- freezer.rb
+++ freezer.rb
@@ -1,42 +1,51 @@
 require_relative "FUNCTIONS.rb"
 include Func
-ACTIONS = [:ADD_ITEM,:ADD_ITEMS_LIST,:REMOVE_ITEM,:REMOVE_ITEMS_LIST,:PURGE,:DEVTOOLS, :HELP, :LIST, :QUIT] #this is all the actions you can use
-CONTAINERS = [:NONE, :CAN, :BAG, :CARTON, :JUG, :GALLON, :PLASTIC_CONTAINER, :PC, 
-:STYROFOAM_CONTAINER, :SC, :BOX, :JAR, :UNDEFINED]
-$buffer = {:test => {:amount => 0, :container => :NONE, :description => 
-"this is a test item that isnt in your freezer, 
-if you are seeing this, you have probs just installed this app, 
-and if that is true, you can probably just use :PURGE 
-and then add the items you have in your freezer."}}
+
+ACTIONS = [:ADD_ITEM,:ADD_ITEMS_LIST,:REMOVE_ITEM,:REMOVE_ITEMS_LIST,:PURGE,:DEVTOOLS, :HELP, :LIST, :QUIT, :SAVE] #this is all the actions you can use
+
+CONTAINERS = [:NONE, :CAN, :BAG, :CARTON, :JUG, :GALLON, :PLASTIC_CONTAINER, :PC, :STYROFOAM_CONTAINER, :SC, :BOX, :JAR, :UNDEFINED]
+
+$buffer = {
+    :test => {
+	:amount => 
+	    0,
+	:container => 
+	    :NONE, 
+	:description =>  
+	    "this is a test item that isnt in your freezer, if you are seeing this, you have probs just installed this app, and if that is true, you can probably just use :PURGE and then add the items you have in your freezer."}}
+
+$to_write = []
 
 begin
     deep_freezer = File.open("contents.store", "r+")
-    contents = deep_freezer.readlines
+    $contents = decrypt(deep_freezer.readlines)
 rescue Errno::ENOENT
     deep_freezer = File.new("contents.store", "w+")
 end
 
 loop do
-puts "what action do you want to perform?"
-p ACTIONS
-print ">"
-user_input = gets.chomp
-user_in = user_input.upcase.gsub(" ", "_").to_sym
-case user_in
-    when :LIST
-    when :HELP
-	help_prompt
-    when :DEVTOOLS
-	dev_tools
-    when :PURGE
-	purge_start(deep_freezer)
-    when :REMOVE_ITEMS_LIST
-    when :REMOVE_ITEM
-    when :ADD_ITEMS_LIST
-    when :ADD_ITEM
-    add_item
-    when :QUIT, :EXIT
-    #do saving stuff
-    exit
-end
+    puts "what action do you want to perform?"
+    p ACTIONS
+    print ">"
+    user_input = gets.chomp
+    user_in = user_input.upcase.gsub(" ", "_").to_sym
+    case user_in
+	when :LIST
+	when :HELP
+	    help_prompt
+	when :DEVTOOLS
+	    dev_tools
+	when :PURGE
+	    purge_start(deep_freezer)
+	when :REMOVE_ITEMS_LIST
+	when :REMOVE_ITEM
+	when :ADD_ITEMS_LIST
+	when :ADD_ITEM
+	    add_item
+	when :QUIT, :EXIT
+	    save
+	    exit
+	when :SAVE
+	    save
+    end
 end
